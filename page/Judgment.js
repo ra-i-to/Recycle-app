330a8f7ffa6f5c34fba841d5cb0965c4371b7736
raito-aug-3 <yamanaka@esupport.co.jp>
2020-12-15 10:32:53 +0900

    test

--- page/Judgment.js
+++ page/Judgment.js
@@ -1,17 +1,5 @@
 import { useState, useEffect } from "react";
-import * as tf from "@tensorflow/tfjs";
-import "@tensorflow/tfjs-react-native";
 
 export default function Judgment() {
-  const [isTfReady, setIsTfReady] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      await tf.ready();
-      // Signal to the app that tensorflow.js can now be used.
-      setIsTfReady(true);
-    })();
-  }, []);
-
   return {};
 }
